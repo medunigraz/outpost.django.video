e2a63786e9cc5b73cb7d97178d43a7f1516e5190
Michael Fladischer <michael@fladi.at>
2023-07-05 08:07:27 +0200

    Add requirement for remote client address in viewer creation request.

--- src/outpost/django/video/migrations/0044_epiphansource_url.py
+++ src/outpost/django/video/migrations/0044_epiphansource_url.py
@@ -13,7 +13,7 @@ def set_url(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("video", "0045_liveviewer_client"),
+        ("video", "0043_auto_20220913_1004"),
     ]
 
     operations = [
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
             name="url",
             field=models.URLField(null=True),
         ),
-        migrations.RunPython(set_url),
+        migrations.RunPython(set_url, lambda *a, **k: None),
         migrations.AlterField(
             model_name="epiphansource",
             name="url",
